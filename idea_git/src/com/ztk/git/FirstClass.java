7e4b535d576fdb67041c52d26d9c7ca633f25516
kunkun <1727245876@qq.com>
2019-12-03 20:44:06 +0800

    第二次修改，新建了一个类

--- idea_git/src/com/ztk/git/FirstClass.java
+++ idea_git/src/com/ztk/git/FirstClass.java
@@ -1,5 +1,14 @@
 package com.ztk.git;
 
+
+class test{
+    public void test(){
+        int i = 10;
+        i += 10;
+        System.out.println("hello，git,我来啦");
+    }
+}
+
 public class FirstClass {
     public static void main(String [] args){
         System.out.println("hello,git project");
@@ -8,11 +17,8 @@ public class FirstClass {
         System.out.println("hello,git project");
         System.out.println("hello,git project");
 
-    }
+        test t = new test();
+        t.test();
 
-    public int test(){
-        int i = 10;
-        i += 10;
-        return i;
     }
 }
